37eeb954d795bf09707a877d93edfb10f9d5debb
om543 <ompardeshi1420@gmail.com>
2026-05-12 20:56:39 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,21 +1,40 @@
-g = {}
+# Depth First Search (DFS) using Recursion
+# Undirected Graph without using class
 
-n = int(input("Nodes: "))
+def add_edge(graph, u, v):
+    # Add nodes if not present
+    if u not in graph:
+        graph[u] = []
 
-for i in range(n):
-    a = input("Node: ")
-    g[a] = input("Neighbours: ").split()
+    if v not in graph:
+        graph[v] = []
 
-v = []
-
-
-def dfs(x):
-    if x not in v:
-        print(x, end=" ")
-        v.append(x)
-
-        for i in g[x]:
-            dfs(i)
+    # Undirected graph
+    graph[u].append(v)
+    graph[v].append(u)
 
 
-dfs(input("Start: "))
+def dfs(graph, node, visited):
+    visited.add(node)
+    print(node, end=" ")
+
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+
+# Main Program
+graph = {}
+
+edges = int(input("Enter number of edges: "))
+
+print("Enter edges (u v):")
+for i in range(edges):
+    u, v = input().split()
+    add_edge(graph, u, v)
+
+start = input("Enter starting vertex: ")
+
+print("\nDFS Traversal:")
+visited = set()
+dfs(graph, start, visited)
