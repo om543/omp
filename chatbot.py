61c0db24f829a7de5784ec77d35a612f5414349c
om543 <ompardeshi1420@gmail.com>
2026-05-12 19:32:02 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,9 @@
-n = int(input("Vertices: "))
+import streamlit as st
+from data import data
 
-g = []
+st.title("Chatbot")
 
-print("Enter matrix:")
+msg = st.text_input("You:")
 
-for i in range(n):
-    g.append(list(map(int, input().split())))
-
-s = [1]+[0]*(n-1)
-
-for k in range(n-1):
-    m = 999
-
-    for i in range(n):
-        if s[i]:
-            for j in range(n):
-                if not s[j] and g[i][j]:
-                    if m > g[i][j]:
-                        m = g[i][j]
-                        x, y = i, j
-
-    print(x, "-", y, "=", g[x][y])
-    s[y] = 1
+if msg:
+    st.write("Bot:", data.get(msg.lower(), "I don't understand"))
